5899e5c98ec6798e4d29dab78b8d0bbf2eef988a
Taha Toklucu <tahatoklucu@hotmail.com>
2025-04-09 13:46:56 +0200

    details - add to cart update
    details - add to cart update

--- ComputerSaleProject/computersale/src/components/ComputerItem.jsx
+++ ComputerSaleProject/computersale/src/components/ComputerItem.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import '../styles/Computers.css'
+import { Link } from 'react-router-dom'
 
 function ComputerItem({image, name, content, price}) {
+
   return (
     <div className='computerItem'>
         <div style={{ backgroundImage: `url(${image})` }}></div>
         <h1>{name}</h1>
         <h6>{content}</h6>
         <p>{price}</p>
+        <div className='buttons'>
+          <button className='addCart'>Add to cart</button>
+          <Link className='details' to="/details">Details</Link>
+        </div>
     </div>
   )
 }
--- ComputerSaleProject/computersale/src/components/Navbar.jsx
+++ ComputerSaleProject/computersale/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import ComputerLogo from '../assets/computerLogo.png';
 import '../styles/Navbar.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Navbar() {
+
+  const [basketCount, setBasketCount] = useState(0);
+
   return (
     <div className='navbar'>
         <div className='main'>
@@ -14,6 +18,8 @@ function Navbar() {
                 <NavLink className="navbar-item" to="/about">About Us</NavLink>
                 <NavLink className="navbar-item" to="/contact">Contact Us</NavLink>
             </div>
+            <span className='basketCount'>{basketCount}</span>
+            <ShoppingBasketIcon style={{fontSize:"24px", marginRight: "20px", color:"#dee0ea"}} />
             <Link className='login-register' to="/login">Log In / Register</Link>
         </div>
     </div>
