864b7669d09e47bd0b66d66ad20e3eee89fab3bc
Taha Toklucu <tahatoklucu@hotmail.com>
2025-04-25 22:28:06 +0200

    search-bar update
    search-bar update

--- ComputerSaleProject/computersale/src/components/Shops.jsx
+++ ComputerSaleProject/computersale/src/components/Shops.jsx
@@ -2,37 +2,87 @@ import React, { useEffect, useState } from 'react'
 import ShopItem from './ShopItem';
 import '../styles/Shops.css';
 import axios from 'axios';
+import { Alert } from 'react-bootstrap';
 
 
 function Shops({setBasketCount}) {
 
-    const [image, setImage] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        setIsLoading(true);
         axios.get("https://api.escuelajs.co/api/v1/products?offset=3&limit=12")
         .then(response => {
-            setImage(response.data);
+            setProducts(response.data);
+            setFilteredProducts(response.data);
+            setIsLoading(false);
         })
         .catch(error => {
             console.error(error);
         })
     },[]);
 
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        try{
+            let response;
+            if(searchTerm.trim() === '') {
+                response = await axios.get("https://api.escuelajs.co/api/v1/products?offset=3&limit=12");
+            } else {
+                response = await axios.get(`https://api.escuelajs.co/api/v1/products/?offset=3&limit=12&title=${searchTerm}`)
+            }
+            setFilteredProducts(response.data);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+   
   return (
     <div className='computers'>
         <h1 className='computerTitle'>Our Products for Sale</h1>
-        <div className='computerList'>
-            {image.map(item => (
-                <ShopItem 
-                    key={item.id}
-                    title = {item.title}
-                    price={item.price}
-                    desc={item.description}
-                    image={item.images}
-                    category={item.category}
-                    onAddToCart = {() => setBasketCount(prev => prev + 1)}
+        <form onSubmit={handleSearch}>
+                <input 
+                    type='text' 
+                    placeholder='Search products by title...'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
-            ))}
+                <button type='submit'>Search</button>
+        </form>
+        <div className='computerList'>
+
+             {isLoading ? (
+                <>
+                {[
+                  'light',
+                ].map((variant) => (
+                  <Alert key={variant} variant={variant}>
+                    Loading...
+                  </Alert>
+                ))}
+              </>
+            ) : (
+                <>
+                    {filteredProducts.map(item => (
+                        <ShopItem
+                            key={item.id}
+                            title={item.title}
+                            price={item.price}
+                            desc={item.description}
+                            image={item.images}
+                            category={item.category}
+                            onAddToCart={() => setBasketCount(prev => prev + 1)}
+                        />
+                    ))}
+                </>
+            )}
         </div>
     </div>
   )
