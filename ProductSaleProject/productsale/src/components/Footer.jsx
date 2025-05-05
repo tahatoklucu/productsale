8662334d8099220a8565dee4e6004c1ed0e1d658
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-05 02:55:41 +0200

    footer-update
    footer-update

--- ProductSaleProject/productsale/src/components/Footer.jsx
+++ ProductSaleProject/productsale/src/components/Footer.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import '../styles/Footer.css';
-import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import XIcon from '@mui/icons-material/X';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Footer() {
   return (
     <div className='footer'>
         <div className='socialIcon'>
-            <FacebookIcon />
-            <InstagramIcon />
-            <XIcon />
+            <a href='https://github.com/tahatoklucu' target='_blank'>
+              <GitHubIcon />
+            </a>
+            <a href='https://www.instagram.com/tahatoklucu/' target='_blank'>
+              <InstagramIcon />
+            </a>
+            <a href='mailto:tahatoklucu@hotmail.com' target='_blank'>
+              <MailOutlineIcon />
+            </a>
         </div>
         <p>© Taha Toklucu. All rights reserved.</p>
     </div>
