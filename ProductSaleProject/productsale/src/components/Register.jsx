bcbd137256422102968cc2df49cbc1ad93a04d22
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-05 03:05:13 +0200

    register-update
    register-update

--- ProductSaleProject/productsale/src/components/Register.jsx
+++ ProductSaleProject/productsale/src/components/Register.jsx
@@ -60,6 +60,7 @@ function Register() {
         });
   return (
     <motion.form onSubmit={handleSubmit} className='register-mainForm' initial={{opacity: 0.7}}  animate={{opacity: 1}} transition={{duration: 0.75}} exit={{opacity: 0}}>
+        <div className='registerTitle'>Register</div>
         <div className='register-div'>
             <label className='register-label'>Username</label>
             <input type='text' value={values.username} onChange={handleChange} id='username' placeholder='Please enter your username' className={errors.username ? ' register-input input-error' : 'register-input'}/>
