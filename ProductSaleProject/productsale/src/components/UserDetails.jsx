ae9b8caa98feab8a38ced97421e1962499eebdc3
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-12 03:05:27 +0200

    UserDetails update
    UserDetails update

--- ProductSaleProject/productsale/src/components/UserDetails.jsx
+++ ProductSaleProject/productsale/src/components/UserDetails.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/UserDetails.css';
+import { motion } from 'framer-motion';
 
 function UserDetails() {
   return (
-    <div className='user-main'>
+    <motion.div className='user-main' initial={{opacity: 0.7}}  animate={{opacity: 1}} transition={{duration: 0.75}} exit={{opacity: 0}}>
         <div className='user-title'>User Details</div>
-    </div>
+    </motion.div>
   )
 }
 
