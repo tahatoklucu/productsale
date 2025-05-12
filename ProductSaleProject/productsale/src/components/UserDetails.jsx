b34e645c21f259d5a4690e12b369dd5fc754f0f3
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-12 03:02:40 +0200

    UserDetails update
    UserDetails update

--- ProductSaleProject/productsale/src/components/UserDetails.jsx
+++ ProductSaleProject/productsale/src/components/UserDetails.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../styles/UserDetails.css';
 
 function UserDetails() {
   return (
-    <div>UserDetails</div>
+    <div className='user-main'>
+        <div className='user-title'>User Details</div>
+    </div>
   )
 }
 
