7a92e7955bcba56b190d422c1c1132e1e8fbc137
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-13 01:27:52 +0200

    UserDetails update
    UserDetails update

--- ProductSaleProject/productsale/src/components/UserDetails.jsx
+++ ProductSaleProject/productsale/src/components/UserDetails.jsx
@@ -6,6 +6,12 @@ function UserDetails() {
   return (
     <motion.div className='user-main' initial={{opacity: 0.7}}  animate={{opacity: 1}} transition={{duration: 0.75}} exit={{opacity: 0}}>
         <div className='user-title'>User Details</div>
+        <div className='details-container'>
+          <div className='details-item'>
+            <label className='label-item'>Name: </label>
+            <span className='detail-exp'>A</span>
+          </div>
+        </div>
     </motion.div>
   )
 }
