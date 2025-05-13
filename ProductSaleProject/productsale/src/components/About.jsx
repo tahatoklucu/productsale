4aef8ed0e6c599c0ae374dab5d30a1ddc7f58a0e
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-13 02:55:04 +0200

    About Page Update
    About Page Update

--- ProductSaleProject/productsale/src/components/About.jsx
+++ ProductSaleProject/productsale/src/components/About.jsx
@@ -11,6 +11,14 @@ function About() {
         <h1>About Us</h1>
         <span className='about-dev'>About Developer</span>
         <p>Hi! I'm Taha Toklucu, a dedicated developer with a strong passion for building intuitive and efficient web applications. My goal is to combine clean code with thoughtful design to deliver engaging user experiences. This project represents my commitment to creating reliable and scalable digital solutions that not only meet today’s needs but are also ready for tomorrow’s challenges.</p>
+        <span className='about-dev'>Technologies I'm Using</span>
+        <ul className='tech'>
+          <li className='tech-item'>React.js for high-performance, dynamic user interfaces</li>
+          <li className='tech-item'>React Bootstrap + custom CSS solutions for responsive, accessible design</li>
+          <li className='tech-item'>Mobile-First Approach delivering flawless UX across all devices</li>
+          <li className='tech-item'>Vercel for continuous integration and instant deployment (CI/CD)</li>
+          <li className='tech-item'>Optimized Performance with fast loading speeds and SEO-friendly architecture</li>
+        </ul>
       </div>
     </motion.div>
   )
