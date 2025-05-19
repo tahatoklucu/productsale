9084f0f79e233b41b551a6bf5398fc046c789cfb
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-19 12:28:05 +0200

    Settings Page Update
    Settings Page Update

--- ProductSaleProject/productsale/src/components/Settings.jsx
+++ ProductSaleProject/productsale/src/components/Settings.jsx
@@ -6,6 +6,15 @@ function Settings({loggedIn}) {
     <>
         <div className='settings-main'>
             <div className='settings-title'>Settings</div>
+            <div className='change-theme-container'>
+                <label className='theme-title'>Theme:</label>
+                <div className='buttons-container'>
+                    <button className='theme-button'></button>
+                    <button className='theme-button2'></button>
+                    <button className='theme-button3'></button>
+                    <button className='theme-button4'></button>
+                </div>
+            </div>
         </div>
     </>
   )
