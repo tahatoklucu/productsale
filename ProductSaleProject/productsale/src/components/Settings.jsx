ff24496ee14d15d4a42080ca57bc693386ce8fcd
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-19 12:29:49 +0200

    Settings Page Update
    Settings Page Update

--- ProductSaleProject/productsale/src/components/Settings.jsx
+++ ProductSaleProject/productsale/src/components/Settings.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Settings.css';
+import { motion } from 'framer-motion';
 
 function Settings({loggedIn}) {
   return (
     <>
-        <div className='settings-main'>
+        <motion.div className='settings-main' initial={{opacity: 0.7}}  animate={{opacity: 1}} transition={{duration: 0.75}} exit={{opacity: 0}}>
             <div className='settings-title'>Settings</div>
             <div className='change-theme-container'>
                 <label className='theme-title'>Theme:</label>
@@ -15,7 +16,7 @@ function Settings({loggedIn}) {
                     <button className='theme-button4'></button>
                 </div>
             </div>
-        </div>
+        </motion.div>
     </>
   )
 }
