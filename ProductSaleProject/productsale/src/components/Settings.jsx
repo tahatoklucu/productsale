e04ef26bf0f0060a239583f19bd0bfe5cd5527e3
Taha Toklucu <tahatoklucu@hotmail.com>
2025-05-21 15:02:43 +0200

    Settings Page Update
    Settings Page Update

--- ProductSaleProject/productsale/src/components/Settings.jsx
+++ ProductSaleProject/productsale/src/components/Settings.jsx
@@ -8,6 +8,7 @@ function Settings({loggedIn}) {
       {loggedIn ? 
         <motion.div className='settings-main' initial={{opacity: 0.7}}  animate={{opacity: 1}} transition={{duration: 0.75}} exit={{opacity: 0}}>
           <div className='settings-title'>Settings</div>
+          <hr style={{color: 'var(--white)', marginTop: '30px'}} />
           <div className='change-theme-container'>
               <label className='theme-title'>Theme:</label>
               <div className='buttons-container'>
@@ -17,6 +18,16 @@ function Settings({loggedIn}) {
                   <button className='theme-button4'></button>
               </div>
           </div>
+          <hr style={{color: 'var(--white)', marginTop: '30px'}} />
+          <div className='delete-container'>
+            <label className='delete-title'>Delete Account:</label>
+            <label className='delete-information-title'>What Happens If You Delete Your Account?</label>
+            <label className='delete-information'>All personal information, profile details, and settings will be erased.</label>
+            <label className='delete-information'>Account deletion is permanent. It is not possible to retrieve or reactivate the account after deletion.</label>
+            <label className='delete-information'>You can create a new account with the same email address, but the old data will not be accessible.</label>
+            <button className='btn btn-danger delete-btn'>Delete Account</button>
+          </div>
+          <hr style={{color: 'var(--white)', marginTop: '30px'}} />
         </motion.div>
         :
         <div className='not-logged-container'>
