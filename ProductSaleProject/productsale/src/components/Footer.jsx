971473b0739ad47fe7b6106ac4b091ebf24f28f2
Taha Toklucu <tahatoklucu@hotmail.com>
2025-06-25 16:31:57 +0200

    footer update
    footer update

--- ProductSaleProject/productsale/src/components/Footer.jsx
+++ ProductSaleProject/productsale/src/components/Footer.jsx
@@ -3,35 +3,22 @@ import '../styles/Footer.css';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { NavLink } from 'react-router-dom'
 
 function Footer() {
   return (
     <div className='footer'>
-      <div className='container'>
-          <div className='pages'>
-            <NavLink to="/about">
-              About Us
-            </NavLink>
-            <NavLink to="/contact">
-              Contact
-            </NavLink>
-          </div>
-          <div className='copyright'>
-            <p>© Taha Toklucu. All rights reserved.</p>
-          </div>
-          <div className='socialIcon'>
-              <a href='https://github.com/tahatoklucu' target='_blank'>
-                <GitHubIcon />
-              </a>
-              <a href='https://www.instagram.com/tahatoklucu/' target='_blank'>
-                <InstagramIcon />
-              </a>
-              <a href='mailto:tahatoklucu@hotmail.com' target='_blank'>
-                <MailOutlineIcon />
-              </a>
-          </div>  
+        <div className='socialIcon'>
+            <a href='https://github.com/tahatoklucu' target='_blank'>
+              <GitHubIcon />
+            </a>
+            <a href='https://www.instagram.com/tahatoklucu/' target='_blank'>
+              <InstagramIcon />
+            </a>
+            <a href='mailto:tahatoklucu@hotmail.com' target='_blank'>
+              <MailOutlineIcon />
+            </a>
         </div>
+        <p>© Taha Toklucu. All rights reserved.</p>
     </div>
   )
 }
